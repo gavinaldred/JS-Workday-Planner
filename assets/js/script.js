c3e94049db22296d4c12113d19e3ad369c3068f7
Gavin Aldred <Gavin.Aldred@nationaltimbergroup.com>
2023-01-21 18:04:38 +0000

    working js now live

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,12 +34,76 @@ $(document).ready(function () {
   
   for (var i = 8; i <= 17; i++) {
     var timeBlock = `<div id="hour${i}" class="row time-block">
-                          <div class="col-md-1 hour">
-                              ${i}:00am
-                          </div>
-                          <textarea class="col-md-10 description"></textarea>
-                          <button class="btn saveBtn col-md-1"><i class="fas fa-save"></i></button>
-                   
-                      </div>`;
+                              <div class="col-md-1 hour">
+                                  ${i}:00am
+                              </div>
+                              <textarea class="col-md-10 description"></textarea>
+                              <button class="btn saveBtn col-md-1"><i class="fas fa-save"></i></button>
+                        </div>`;
     $(".container").append(timeBlock);
+}
+
+  
+  // Create a function to track the hour
+  function hourTracker() {
+    // Get the current number of hours
+    var currentHour = moment().hour();
+  
+    $(".saveBtn").on("click", function () {
+      var text = $(this).siblings("textarea").val();
+      var hour = $(this).parent().attr("id");
+      localStorage.setItem(hour, text);
+    });
+
+    $(".time-block").each(function () {
+        var hour = $(this).attr("id");
+        var text = localStorage.getItem(hour);
+        $(this).find("textarea").val(text);
+      });
+      
+  
+    $(".clearBtn").on("click", clearPage);
+    $(".refreshBtn").on("click", refreshPage);
+  
+    function clearPage() {
+      localStorage.clear();
+      $(".past")
+        .find("textarea")
+        .removeAttr("disabled")
+        .val("")
+        .attr("placeholder", "");
+    }
+  
+    function refreshPage() {
+        location.reload();
+    }
+    
+    // Loop over time blocks
+    $(".time-block").each(function () {
+      var blockHour = parseInt($(this).attr("id").replace("hour", ""));
+  
+      // Check if we've moved past this time
+      if (blockHour < currentHour) {
+        $(this).addClass("past");
+        $(this).removeClass("future");
+        $(this).removeClass("present");
+        var text = $(this).find("textarea").val();
+        if (!text) {
+          $(this).find("textarea").attr("disabled", "disabled");
+          $(this)
+            .find("textarea")
+            .attr("placeholder", "Time has passed. This block is non-editable.");
+        }
+      } else if (blockHour === currentHour) {
+        $(this).removeClass("past");
+        $(this).addClass("present");
+        $(this).removeClass("future");
+      } else {
+        $(this).removeClass("present");
+        $(this).removeClass("past");
+        $(this).addClass("future");
+      }
+    });
   }
+  hourTracker(); // Re-run the function
+  
